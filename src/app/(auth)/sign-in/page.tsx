b685dd312a271922933f7bd0ee4741a0cb14b670
sahilkhan8786 <official.khansahil8786@gmali.com>
2025-06-13 10:47:22 +0530

    removed all auth functionality

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,15 +1,25 @@
-import { SignInWithCredentials } from '@/components/custom/auth/sign-in-with-credentials'
+// import { SignInWithCredentials } from '@/components/custom/auth/sign-in-with-credentials'
 
-import SignInWithGoogle from '@/components/custom/auth/sign-in-with-google'
+// import SignInWithGoogle from '@/components/custom/auth/sign-in-with-google'
+// import React from 'react'
+
+// const SignInPage = () => {
+//     return (
+//         <div className='h-screen mt-48'>
+//             <SignInWithGoogle />
+//             <SignInWithCredentials />
+//         </div>
+//     )
+// }
+
+// export default SignInPage;
+
 import React from 'react'
 
 const SignInPage = () => {
     return (
-        <div className='h-screen mt-48'>
-            <SignInWithGoogle />
-            <SignInWithCredentials />
-        </div>
+        <div>SignInPage</div>
     )
 }
 
-export default SignInPage;
+export default SignInPage
--- src/components/custom/auth/sign-in-with-credentials.tsx
+++ src/components/custom/auth/sign-in-with-credentials.tsx
@@ -1,22 +1,22 @@
-import { signIn } from "@/auth"
+
 
 export async function SignInWithCredentials() {
     return (
-        <form
-            action={async (formData) => {
-                "use server"
-                await signIn("credentials", formData)
-            }}
-        >
-            <label>
-                Email
-                <input name="email" type="email" />
-            </label>
-            <label>
-                Password
-                <input name="password" type="password" />
-            </label>
-            <button>Sign In</button>
-        </form>
+        // <form
+        //     action={async (formData) => {
+        //         "use server"
+        //         await signIn("credentials", formData)
+        //     }}
+        // >
+        // <label>
+        //     Email
+        //     <input name="email" type="email" />
+        // </label>
+        // <label>
+        //     Password
+        //     <input name="password" type="password" />
+        // </label>
+        <button>Sign In</button>
+        // </form>
     )
 }
