00e0e6c304c6e9f38108f9ba31c11570cdb39de5
sahilkhan8786 <official.khansahil8786@gmali.com>
2025-07-01 09:40:32 +0530

    updated the single product route

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -1,21 +1,20 @@
 import Product from "@/models/product.model";
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(req: NextRequest,
-    {params}:{params:Promise<{slug:string}>}
-) {
-    
-    const slug = (await params).slug;
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { slug: string } }
+  ) {
+    const { slug } = params;
+  
     if (!slug) {
-        return NextResponse.json({ error: "ID is required" }, { status: 400 });
+      return NextResponse.json({ error: "Slug is required" }, { status: 400 });
     }
-
-        const product = await Product.findOne({slug})
-
-        return NextResponse.json({
-            status: 'success',
-            data: {
-                product
-            }
-        })
-}
-
+  
+    const product = await Product.findOne({ slug });
+  
+    return NextResponse.json({
+      status: 'success',
+      data: { product },
+    });
+  }
+  
