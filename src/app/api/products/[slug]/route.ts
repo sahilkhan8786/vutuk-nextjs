cdf95f58ef2c8e54e7307c73fc285709c597b6ee
sahilkhan8786 <official.khansahil8786@gmali.com>
2025-07-01 09:42:08 +0530

    fixed typo

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -1,20 +1,20 @@
 import Product from "@/models/product.model";
 import { NextRequest, NextResponse } from "next/server";
 export async function GET(
-    req: NextRequest,
-    { params }: { params: { slug: string } }
-  ) {
-    const { slug } = params;
-  
-    if (!slug) {
-      return NextResponse.json({ error: "Slug is required" }, { status: 400 });
-    }
-  
-    const product = await Product.findOne({ slug });
-  
-    return NextResponse.json({
-      status: 'success',
-      data: { product },
-    });
+  req: NextRequest,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+  const slug =(await params).slug;
+
+  if (!slug) {
+    return NextResponse.json({ error: "Slug is required" }, { status: 400 });
   }
-  
+
+  const product = await Product.findOne({ slug });
+
+  return NextResponse.json({
+    status: 'success',
+    data: { product },
+  });
+}
+
