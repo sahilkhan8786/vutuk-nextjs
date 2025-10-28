b831ad3695e389cfa67fe93b2a0a0dd1d12d31b8
sahilkhan8786 <official.khansahil8786@gmali.com>
2025-10-28 12:20:16 +0530

    logging the users location

--- src/lib/getIP.ts
+++ src/lib/getIP.ts
@@ -1,5 +1,5 @@
 export async function isIndian() {
-    const res = await fetch(`https://ipwho.is/me`);
+    const res = await fetch(`https://ipwho.is/`);
     const data = await res.json();
     console.log(data)
     return data.country_code === 'IN';
