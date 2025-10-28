983b372b1fbd1af146c7156777c2d7fe734d290f
sahilkhan8786 <official.khansahil8786@gmali.com>
2025-10-28 12:17:02 +0530

    updated getting IP

--- src/lib/getIP.ts
+++ src/lib/getIP.ts
@@ -1,5 +1,6 @@
 export async function isIndian() {
-    const res = await fetch(`https://ipwho.is/`);
+    const res = await fetch(`https://ipwho.is/me`);
     const data = await res.json();
+    console.log(data)
     return data.country_code === 'IN';
 }
